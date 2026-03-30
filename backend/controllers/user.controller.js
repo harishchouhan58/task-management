041ffea1c81f2820f4bae8b97ab689c498571932
harishchouhan58 <157677848+harishchouhan58@users.noreply.github.com>
2026-03-30 17:23:10 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -80,7 +80,8 @@ export const loginUser = async (req, res) => {
 
     const options = {
         httpOnly:true,
-        secure: true
+        secure: true,
+        sameSite: "none"
     }
 
     return res.status(200)
